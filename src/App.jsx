19b7be41b5ee805306ff97e19827c8a545c0d97e
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-07-13 19:04:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Landing, Detail } from "./Pages";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        newestOnTop={false}
+        closeOnClick
+        theme="colored"
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        autoClose={5000}
+      />
+      <div className="bg-stone-700">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
+
+export default App;
